8493e247b69f99a81e13a06ccd15d51216b9025e
Siraj <sirajevans@gmail.com>
2025-06-11 00:25:37 +0100

    Update HeroPattern component's background gradient colors for improved visual appeal and consistency; refactor Logo component to enhance scalability with new SVG design.

--- src/components/HeroPattern.tsx
+++ src/components/HeroPattern.tsx
@@ -4,7 +4,7 @@ export function HeroPattern() {
   return (
     <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
       <div className="absolute top-0 left-1/2 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
-        <div className="absolute inset-0 bg-gradient-to-r from-[#8B5CF6] to-[#A855F7] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#8B5CF6]/30 dark:to-[#A855F7]/30 dark:opacity-100">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#5515F1] via-[#6c31ff] to-[#09050a] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#5515F1]/30 dark:via-[#6c31ff]/30 dark:to-[#09050a]/30 dark:opacity-100">
           <GridPattern
             width={72}
             height={56}
